ed38be03d467401f4bf4b6dcde5e8e4ba9b6c516
phpilatso <phenyopilatso@momentum.co.za>
2020-10-29 10:51:32 +0200

    Update to use current BOM version 11. closes #34

---
+++ src/main/java/guru/sfg/common/events/BeerEvent.java
@@ -0,0 +1,19 @@
+package guru.sfg.common.events;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@Data
+@AllArgsConstructor
+@Builder
+@NoArgsConstructor
+public class BeerEvent implements Serializable {
+
+    static final long serialVersionUID = -5218034560706536178L;
+
+    private BeerDto beerDto;
+}
